83124b559134ddbff651bcfa96c733b932e7c7b3
Ed wander <edwandersilva@gmail.com>
2022-06-27 23:18:25 -0300

    Segundo Commit
    Inserindo a classe diretor. Essa classe faz a separação salarial do cargo de diretoria

--- bytebank_ADM/Program.cs
+++ bytebank_ADM/Program.cs
@@ -4,16 +4,26 @@ using System;
       
             Console.WriteLine("Boas Vindas ao ByteBank Admnistração");
 
-Funcionario pedro = new Funcionario(1);
+Funcionario pedro = new Funcionario();
 
 pedro.Nome = "Pedro";
 pedro.Cpf = "123456789";
 pedro.Salario = 2000;
 
-Console.WriteLine("Nome: " + pedro.Salario);
+Console.WriteLine("Nome: " + pedro.Nome);
 Console.WriteLine( "CPF: "+pedro.Cpf);
 Console.WriteLine("Salário: "+ pedro.Salario);
-Console.WriteLine("Bonificação:"+pedro.getBonificacao());
-        
-    
+
+
+Diretor paula = new Diretor();
+paula.Nome = "Paula";
+paula.Cpf = "125555562";
+paula.Salario = 5000;
+
+Console.WriteLine("Bonificação:" + pedro.getBonificacao());
+Console.WriteLine("Bonificação:" + paula.getBonificacao());
+
+
+
+
 
--- bytebank_ADM/funcionarios/Diretor.cs
+++ bytebank_ADM/funcionarios/Diretor.cs
@@ -6,22 +6,20 @@ using System.Threading.Tasks;
 
 namespace bytebank_ADM.funcionarios
 {
-   public  class Funcionario
+    public class Diretor
     {
-        //private int _tipo;
-      
-        public string  Nome { get; set; }
-       
-         public string Cpf { get; set; }
-        
-         public  double Salario { get; set; }
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public double Salario { get; set; }
 
         public double getBonificacao()
         {
-            
+         
                 return Salario * 0.1;
             }
-            
+
         }
-           
     }
+
