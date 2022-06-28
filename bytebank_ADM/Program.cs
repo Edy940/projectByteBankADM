4000f0bde9cba4e07cf6e155fd742e2402d6d39a
Ed wander <edwandersilva@gmail.com>
2022-06-27 23:02:02 -0300

    Initial Comit
    Primeiro repositório criado para fins de treinamento em CSharp

---
+++ bytebank_ADM/Program.cs
@@ -0,0 +1,19 @@
+﻿using bytebank_ADM.funcionarios;
+using System;
+
+      
+            Console.WriteLine("Boas Vindas ao ByteBank Admnistração");
+
+Funcionario pedro = new Funcionario(1);
+
+pedro.Nome = "Pedro";
+pedro.Cpf = "123456789";
+pedro.Salario = 2000;
+
+Console.WriteLine("Nome: " + pedro.Salario);
+Console.WriteLine( "CPF: "+pedro.Cpf);
+Console.WriteLine("Salário: "+ pedro.Salario);
+Console.WriteLine("Bonificação:"+pedro.getBonificacao());
+        
+    
+
---
+++ bytebank_ADM/funcionarios/Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.funcionarios
+{
+   public  class Funcionario
+    {
+        private int _tipo;
+
+        public Funcionario(int _tipo)
+        {
+            this._tipo = _tipo;
+        }
+
+        public string  Nome { get; set; }
+       
+         public string Cpf { get; set; }
+        
+         public  double Salario { get; set; }
+
+        public double getBonificacao()
+        {
+            if(_tipo == 1)
+            {
+                return Salario;
+            }
+            else
+            {
+                return Salario * 0.1;
+            }
+            
+        }
+           
+    }
+}
